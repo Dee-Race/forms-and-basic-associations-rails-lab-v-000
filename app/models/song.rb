2bfbc181e6fde2c9d22d6bfc0f35f36eb6fea171
Dee Race <deerace12@gmail.com>
2018-06-19 20:11:05 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,10 +5,9 @@ class Song < ActiveRecord::Base
 
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
-  end 
+  end
 
   def genre_name
-    self.genre ? self.genre.name : nil 
+    self.genre ? self.genre.name : nil
   end
 end
-
